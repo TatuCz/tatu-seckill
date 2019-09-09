fe08ef5928ace538b7310a859e2debc92b9db6f0
zihui <chenzh11@lenovo.com>
2019-09-10 00:30:45 +0800

    增加统一异常处理

--- src/main/java/com/tatu/seckill/response/ErrorCodeMsg.java
+++ src/main/java/com/tatu/seckill/response/ErrorCodeMsg.java
@@ -8,6 +8,9 @@ public class ErrorCodeMsg {
     private String msg;
 
     public static ErrorCodeMsg SERVER_ERROR = new ErrorCodeMsg(100100, "服务器错误");
+    public static ErrorCodeMsg BIND_EXCEPTION = new ErrorCodeMsg(100101, "参数校验异常. %s");
+
+
     public static ErrorCodeMsg USER_NOT_FOUNT = new ErrorCodeMsg(100201, "用户不存在");
 
     public static ErrorCodeMsg NICKNAME_EMPTY = new ErrorCodeMsg(100300, "登陆用户名不能为空");
@@ -17,5 +20,10 @@ public class ErrorCodeMsg {
     private ErrorCodeMsg(int code, String msg) {
         this.code = code;
         this.msg = msg;
+    }
+
+    public ErrorCodeMsg fillArgs(Object... args) {
+        this.msg = String.format(this.msg, args);
+        return this;
     }
 }
